fb1b643cb99977730a9b718fd71cab15facaf8c6
Konstantin Melnikov <k.melnikov@primo-rpa.ru>
2025-11-09 18:20:03 +0300

    task 13

--- task_13/main.go
+++ task_13/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	variant1()
 	variant2()
+	variant3()
 }
 
 func variant1() {
@@ -27,6 +28,19 @@ func variant2() {
 	a = a ^ b
 	b = a ^ b
 	a = a ^ b
+
+	fmt.Println("a: ", a, " b: ", b)
+}
+
+func variant3() {
+	a := 5
+	b := 1
+
+	fmt.Println("a: ", a, " b: ", b)
+
+	a = a + b
+	b = a - b
+	a = a - b
 
 	fmt.Println("a: ", a, " b: ", b)
 }
